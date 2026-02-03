094305b984b939c22c1e686746defe97a55098be
Matthias Wessendorf <mwessend@redhat.com>
2026-02-03 16:34:17 +0100

    Make usage of new kube-extension operations for kubeconfig
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- evals/tasks/config/identify-context-for-task/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# No cleanup needed - we didn't create any resources
-echo "No cleanup required for config inspection task"
--- evals/tasks/config/list-contexts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# This task works with the MCP server's current kubeconfig
-# No setup needed - the agent will query the actual kubeconfig via MCP tools
-echo "Using MCP server's current kubeconfig (no setup required)"
