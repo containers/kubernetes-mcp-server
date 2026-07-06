aed13fc847e771bb2dd9c4ed33800741fe5a95e0
Anand Kumar Singh <anandrkskd@gmail.com>
2026-07-06 13:37:45 +0530

    remove argocd toolset in favor of generic tools-set
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>

--- pkg/mcp/modules.go
+++ pkg/mcp/modules.go
@@ -1,7 +1,6 @@
 package mcp
 
 import (
-	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/argocd"
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/config"
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/core"
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/helm"
